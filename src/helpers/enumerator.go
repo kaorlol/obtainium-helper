eefb71c53e8af26042e0aaf437c84ced3e9e5ef2
jacoby <jacobyb0508@gmail.com>
2024-06-06 20:36:49 -0400

    debug prints

--- src/helpers/enumerator.go
+++ src/helpers/enumerator.go
@@ -83,6 +83,7 @@ func Enumerate(ctx context.Context, download utils.Download) (string, error) {
 
 					newIdentifier := identifierToString(currentIdentifier)
 					resp, err := utils.Request(fmt.Sprintf("%s%s.apk", download.URL, newIdentifier))
+					println(fmt.Sprintf("%s%s.apk", download.URL, newIdentifier))
 					if err != nil {
 						continue
 					}
